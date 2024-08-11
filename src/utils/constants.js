35a18817a9e4740f9c34c83cd34acd56c4177ce0
harun-u-r-rashid <hr100756@gmail.com>
2024-08-12 05:13:26 +0600

    url update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 import UserData from "../views/plugin/UserData";
 // https://education-platfrom-u6f9.onrender.com/
 // http://127.0.0.1:8000/api/
-export const API_BASE_URL = `https://education-platfrom-u6f9.onrender.com/`;
+export const API_BASE_URL = `https://education-platfrom-u6f9.onrender.com/api/`;
+// export const API_BASE_URL = `http://127.0.0.1:8000/api/`;
 export const userId = UserData()?.user_id;
 export const PAYPAL_CLIENT_ID = "test";
 
