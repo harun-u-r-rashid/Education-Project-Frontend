3d3a0f6d331ad9273452070fe1fc1438021048b1
harun-u-r-rashid <hr100756@gmail.com>
2024-09-01 02:59:00 +0600

    api url update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 import UserData from "../views/plugin/UserData";
 
 
-// export const API_BASE_URL = `https://academy-platfrom-u6f9.onrender.com/api/`;
-export const API_BASE_URL = `http://127.0.0.1:8000/api/`;
+export const API_BASE_URL = `https://academy-platfrom-u6f9.onrender.com/api/`;
+// export const API_BASE_URL = `http://127.0.0.1:8000/api/`;
 export const userId = UserData()?.user_id;
 export const isActive = UserData()?.is_active;
 
